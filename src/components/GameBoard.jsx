7ba62028da7fed7a92c6d8a12f6a48838d56a933
Hayden Taylor <hta55@uclive.ac.nz>
2019-03-18 23:43:51 +1300

    added locale

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import styled from "styled-components";
+import axios from "axios";
 
 import BoardTile from "./BoardTile";
-import { days, tileInformation } from "../static/TileObjects";
+import {
+  days,
+  MailImage,
+  WhitecrowImage,
+  PiggyBankImage,
+  BriefCaseImage,
+  SunImage,
+  SleepImage,
+  BeachImage,
+  MoonImage
+} from "../static/TileObjects";
 
 const PageTitle = styled.h1`
   color: white;
@@ -46,28 +57,62 @@ export default class GameBoard extends React.Component {
   componentDidMount() {
     let week = [];
     let weeks = [];
-    for (var j = 1; j <= 35; j++) {
-      week.push(
-        <BoardTile
-          key={j}
-          color={tileInformation[j - 1].color}
-          title={tileInformation[j - 1].title}
-          description={tileInformation[j - 1].description}
-          action={tileInformation[j - 1].action}
-          date={tileInformation[j - 1].date}
-          dateColor={tileInformation[j - 1].dateColor}
-          dateTextColor={tileInformation[j - 1].dateTextColor}
-          descriptionColor={tileInformation[j - 1].descriptionColor}
-          image={tileInformation[j - 1].image}
-        />
-      );
-      if (j % 7 === 0) {
-        weeks.push(<Wrapper>{week}</Wrapper>);
-        week = [];
-      }
-    }
-    this.setState({ weeks: weeks });
+
+    axios
+      .get("/board?lang=en")
+      .then(response => {
+        response = this.mapImageToTile(response.data);
+        this.stripTitles(response);
+        for (var j = 1; j <= 35; j++) {
+          week.push(
+            <BoardTile
+              key={j}
+              color={response[j - 1].color}
+              title={response[j - 1].title}
+              description={response[j - 1].description}
+              action={response[j - 1].action}
+              date={response[j - 1].date}
+              dateColor={response[j - 1].dateColor}
+              dateTextColor={response[j - 1].dateTextColor}
+              descriptionColor={response[j - 1].descriptionColor}
+              image={response[j - 1].image}
+            />
+          );
+          if (j % 7 === 0) {
+            weeks.push(<Wrapper>{week}</Wrapper>);
+            week = [];
+          }
+        }
+        this.setState({ weeks: weeks });
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
+
+  stripTitles = data => {
+    data[34].date = null;
+    data[33].date = null;
+  };
+
+  mapImageToTile = data => {
+    let mail = [1, 3, 5, 11, 16, 19, 24, 26];
+    mail.forEach(index => {
+      data[index].image = MailImage;
+    });
+
+    data[31].image = WhitecrowImage;
+    data[29].image = BriefCaseImage;
+
+    //Rest Days
+    data[7].image = SunImage;
+    data[14].image = BeachImage;
+    data[21].image = SleepImage;
+    data[28].image = MoonImage;
+
+    data[34].image = PiggyBankImage;
+    return data;
+  };
 
   render() {
     return (
--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import GameBoard from "./components/GameBoard";
 import "./index.css";
+import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost:8080";
 
 ReactDOM.render(<GameBoard />, document.getElementById("root"));
